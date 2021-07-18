3cdbd865e89a1bb72e1b47f71630fbdd47b6e50b
Kristian Hovland <32363483+kristianhovland@users.noreply.github.com>
2021-07-18 22:43:58 +0200

    Delete src directory

--- src/components/Error.js
+++
@@ -1,5 +0,0 @@
-import Alert from "react-bootstrap/Alert";
-
-export default function ErrorMessage({ message }) {
-  return <Alert variant="danger">{message}</Alert>;
-}
--- src/components/Loader.js
+++
@@ -1,9 +0,0 @@
-import Spinner from "react-bootstrap/Spinner";
-
-export default function Loader() {
-  return (
-    <div className="spinner-container">
-      <Spinner animation="grow" variant="info" />
-    </div>
-  );
-}
